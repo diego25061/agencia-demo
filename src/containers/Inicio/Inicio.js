60a67d67333e1164bdeb995e85a7fdc57b99b4b8
Diego Ruiz <diego25061@gmail.com>
2019-06-14 00:12:18 -0500

    Carousel e imagenes agregados

--- src/containers/Inicio/Inicio.js
+++ src/containers/Inicio/Inicio.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import {Component} from 'react';
+import LogoYllari from '../../assets/logo_yllari.png'
+
+import Huascaran from '../../assets/vistas/huascaran.png'
+import Machupichu from '../../assets/vistas/machupichu.png'
+import Costaverde from '../../assets/vistas/costaverdehd.png'
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 class Inicio extends Component{
 
@@ -9,7 +17,28 @@ class Inicio extends Component{
     }
     
     render(){
-        return <div><h3>Pagina web en desarrollo :3</h3></div>
+        //return <div><h3>Pagina web en desarrollo :3</h3></div>
+        
+        return <div>
+            <br/>
+            <br/>
+            <Carousel showThumbs={false} showArrows={true} 
+            autoPlay
+            stopOnHover
+            infiniteLoop
+            interval={8000}
+            emulateTouch={true}>
+            <div>
+                <img src={Huascaran} />
+            </div>
+            <div>
+                <img src={Machupichu} />
+            </div>
+            <div>
+                <img src={Costaverde} />
+            </div>
+        </Carousel><br/><br/>
+        </div>
     }
 }
 
